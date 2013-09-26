a5e31c101078e27eada5340ccdf194affeb249e3
Thomas Appel <mail@thomas-appel.com>
2013-09-26 09:44:50 +0200

    add value type conversion

--- lib/xmltoarray.php
+++ lib/xmltoarray.php
@@ -21,6 +21,8 @@ class XmlToArray implements InterfaceParser
      */
     protected $xml;
 
+    protected $xmlErrors;
+
     /**
      * __construct
      *
@@ -40,10 +42,66 @@ class XmlToArray implements InterfaceParser
      */
     public function parse()
     {
-        $xmlObj = simplexml_load_string($this->xml, null, LIBXML_DTDATTR);
+        if (!$xmlObj = $this->loadXml($this->xml)) {
+            return $this->xmlErrors;
+        }
+
         $root = $xmlObj->getName();
         $data = $this->parseXMLOBJ($xmlObj);
         return array($root => $data);
+    }
+
+    protected function loadXml($xml)
+    {
+        $usedInternalErrors = libxml_use_internal_errors(true);
+        $externalEntitiesDisabled = libxml_disable_entity_loader(false);
+        libxml_clear_errors();
+
+        set_error_handler(array($this, 'handleXmlErrors'));
+
+        if (!$xmlObj = simplexml_load_string($this->xml, null, LIBXML_NONET|LIBXML_DTDATTR)) {
+            $this->resetErrors($usedInternalErrors, $externalEntitiesDisabled);
+            return $this->handleXmlErrors(libxml_get_errors());
+        }
+
+        restore_error_handler();
+
+        return $xmlObj;
+    }
+
+    /**
+     * resetErrors
+     *
+     * @param mixed $usedInternalErrors
+     * @param mixed $externalEntitiesDisabled
+     *
+     * @access protected
+     * @return mixed
+     */
+    protected function resetErrors($usedInternalErrors, $externalEntitiesDisabled)
+    {
+        libxml_use_internal_errors($usedInternalErrors);
+        libxml_disable_entity_loader($externalEntitiesDisabled);
+    }
+
+    /**
+     * handleXmlErrors
+     *
+     * @param array $errors
+     *
+     * @access protected
+     * @return mixed
+     */
+    protected function handleXmlErrors(array $errors)
+    {
+        $err = array();
+
+        foreach ($errors as $i => $error) {
+            $err[$i] = $error->message;
+        }
+
+        $this->xmlErrors = array('error' => 'xml error');
+        return false;
     }
 
     /**
@@ -96,14 +154,27 @@ class XmlToArray implements InterfaceParser
             $result = array_merge($attributes, $result);
             return $result;
 
-        } else if (!is_null($text)) {
+        } elseif (!is_null($text)) {
             if (!empty($result)) {
                 $result['text'] = $text;
             } else {
-                $result = $text;
+                $result = $this->getValue($text);
             }
             return $result;
         }
         return (empty($result) && is_null($text)) ? null : $result;
+    }
+
+    private function getValue($value)
+    {
+        if (is_numeric($value)) {
+            if (false !== strpos($value, '.')) {
+                return (float)$value;
+            }
+            return (int)$value;
+        } elseif (in_array(strtolower($v = $value), array('true', 'false'))) {
+            return 'false' === $v ? false : true;
+        }
+        return $value;
     }
 }
